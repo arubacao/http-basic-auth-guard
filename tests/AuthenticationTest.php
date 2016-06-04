01826e5b9906b33d7ed318976de5e50d70b712ca
arubacao <arubacao@gmail.com>
2016-06-04 13:32:01 -0400

    Applied fixes from StyleCI

--- tests/AuthenticationTest.php
+++ tests/AuthenticationTest.php
@@ -7,7 +7,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace Arubacao\Tests\BasicAuth;
 
 class AuthenticationTest extends AbstractTestCase
@@ -46,14 +45,14 @@ class AuthenticationTest extends AbstractTestCase
     public function user_gets_ok_with_correct_credentials()
     {
         $user = factory(\App\User::class)->create([
-            'password' => bcrypt('123456')
+            'password' => bcrypt('123456'),
         ]);
         $this->app->router->get('api/whatever', ['middleware' => 'auth:api', function () {
             return 'YoYo!';
         }]);
         $this->json('GET', 'api/whatever', [], [
             'PHP_AUTH_USER' => $user->email,
-            'PHP_AUTH_PW' => '123456'
+            'PHP_AUTH_PW' => '123456',
         ])
             ->seeStatusCode(200);
     }
