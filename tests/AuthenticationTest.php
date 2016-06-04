f951885eba270af342f26546ea00355d1222394b
arubacao <arubacao@gmail.com>
2016-06-04 13:47:36 -0400

    Applied fixes from StyleCI

--- tests/AuthenticationTest.php
+++ tests/AuthenticationTest.php
@@ -7,7 +7,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace Arubacao\Tests\BasicAuth;
 
 class AuthenticationTest extends AbstractTestCase
