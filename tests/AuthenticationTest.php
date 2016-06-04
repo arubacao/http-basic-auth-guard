0876d83b53cbc582ead05beac661f61bd6d267b2
arubacao <arubacao@gmail.com>
2016-06-04 19:38:21 +0200

    fix testsuite

--- tests/AuthenticationTest.php
+++ tests/AuthenticationTest.php
@@ -24,6 +24,7 @@ class AuthenticationTest extends AbstractTestCase
         }]);
         $this->json('GET', 'api/whatever', [], [
             'PHP_AUTH_USER' => $user->email,
+            'PHP_AUTH_PW' => null,
         ])
             ->seeStatusCode(401);
     }
@@ -46,14 +47,14 @@ class AuthenticationTest extends AbstractTestCase
     public function user_gets_ok_with_correct_credentials()
     {
         $user = factory(\App\User::class)->create([
-            'password' => bcrypt('123456')
+            'password' => bcrypt('123456'),
         ]);
         $this->app->router->get('api/whatever', ['middleware' => 'auth:api', function () {
             return 'YoYo!';
         }]);
         $this->json('GET', 'api/whatever', [], [
             'PHP_AUTH_USER' => $user->email,
-            'PHP_AUTH_PW' => '123456'
+            'PHP_AUTH_PW' => '123456',
         ])
             ->seeStatusCode(200);
     }
