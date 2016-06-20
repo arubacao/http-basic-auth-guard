0051ec26fddceafdb8a3f88f31a94c16b2aa45c5
arubacao <arubacao@gmail.com>
2016-06-20 09:39:12 -0400

    Applied fixes from StyleCI

--- tests/database/factories/ModelFactory.php
+++ tests/database/factories/ModelFactory.php
@@ -7,7 +7,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 $factory->define(User::class, function (Faker\Generator $faker) {
     return [
         'email' => $faker->safeEmail,
