f17fc065a0ea223d626a0e5b99307114d055c96d
Ivan Djurdjevac <djurdjevac@gmail.com>
2017-10-11 14:47:43 +0000

    Change bcrypt method with password_hash

--- tests/AuthenticationTest.php
+++ tests/AuthenticationTest.php
@@ -8,9 +8,9 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-use Illuminate\Auth\Events\Attempting;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Events\Attempting;
 
 class AuthenticationTest extends AbstractTestCase
 {
@@ -46,7 +46,7 @@ class AuthenticationTest extends AbstractTestCase
     public function user_gets_ok_with_correct_credentials_with_auth_middleware()
     {
         $user = factory(User::class)->create([
-            'password' => bcrypt('123456'),
+            'password' => Hash::make('123456'),
         ]);
         $this->app->router->get('api/whatever', ['middleware' => 'auth:api', function () {
             return 'YoYo!';
@@ -63,7 +63,7 @@ class AuthenticationTest extends AbstractTestCase
     public function attempting_event_gets_fired_with_correct_credentials_with_auth_middleware()
     {
         $user = factory(User::class)->create([
-            'password' => bcrypt('123456'),
+            'password' => Hash::make('123456'),
         ]);
         $this->app->router->get('api/whatever', ['middleware' => 'auth:api', function () {
             return 'YoYo!';
@@ -81,7 +81,7 @@ class AuthenticationTest extends AbstractTestCase
     public function login_event_not_fired_with_correct_credentials_with_auth_middleware()
     {
         $user = factory(User::class)->create([
-            'password' => bcrypt('123456'),
+            'password' => Hash::make('123456'),
         ]);
         $this->app->router->get('api/whatever', ['middleware' => 'auth:api', function () {
             return 'YoYo!';
@@ -99,7 +99,7 @@ class AuthenticationTest extends AbstractTestCase
     public function basic_method_fires_attempting_event_with_valid_credentials()
     {
         $user = factory(User::class)->create([
-            'password' => bcrypt('123456'),
+            'password' => Hash::make('123456'),
         ]);
 
         $this->app->router->get('api/whatever', function () {
@@ -133,7 +133,7 @@ class AuthenticationTest extends AbstractTestCase
     public function basic_method_fires_login_event_with_valid_credentials()
     {
         $user = factory(User::class)->create([
-            'password' => bcrypt('123456'),
+            'password' => Hash::make('123456'),
         ]);
 
         $this->app->router->get('api/whatever', function () {
@@ -167,7 +167,7 @@ class AuthenticationTest extends AbstractTestCase
     public function onceBasic_method_does_not_fire_login_event_with_valid_credentials()
     {
         $user = factory(User::class)->create([
-            'password' => bcrypt('123456'),
+            'password' => Hash::make('123456'),
         ]);
 
         $this->app->router->get('api/whatever', function () {
--- tests/database/factories/ModelFactory.php
+++ tests/database/factories/ModelFactory.php
@@ -10,6 +10,6 @@
 $factory->define(User::class, function (Faker\Generator $faker) {
     return [
         'email' => $faker->safeEmail,
-        'password' => bcrypt($faker->password),
+        'password' => Hash::make($faker->password),
     ];
 });
