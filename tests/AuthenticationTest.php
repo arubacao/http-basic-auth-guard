9b9e602e59dc4df53508b00d0090f78cd713d9b2
arubacao <arubacao@users.noreply.github.com>
2017-10-17 15:39:18 +0200

    Update AuthenticationTest.php

--- tests/AuthenticationTest.php
+++ tests/AuthenticationTest.php
@@ -10,6 +10,7 @@
  */
 use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Auth\Events\Attempting;
 
 class AuthenticationTest extends AbstractTestCase
